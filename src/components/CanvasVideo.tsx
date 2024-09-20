258e7a20bd0bacc8277b4f93f3caf5cf5b06ea29
Nilesh Darji <nilesh.28.2003@gmail.com>
2024-09-20 21:55:20 +0530

    created scroll animated video

---
+++ src/components/CanvasVideo.tsx
@@ -0,0 +1,114 @@
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const CanvasVideo = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const context = canvas.getContext("2d");
+    if (!context) return;
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const resizeCanvas = () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      render();
+    };
+
+    window.addEventListener("resize", resizeCanvas);
+
+    const frameCount = 206;
+    const images: HTMLImageElement[] = [];
+    const imageSeq = { frame: 1 };
+
+    for (let i = 0; i < frameCount; i++) {
+      const img = new Image();
+      img.src = files(i);
+      images.push(img);
+    }
+
+    const ctl = gsap.timeline({
+      scrollTrigger: {
+        scrub: 0.15,
+        trigger: `#canvas-card>canvas`,
+        start: `top top`,
+        end: `300% top`,
+        scroller: `body`,
+      },
+    });
+
+    ctl
+      .to(imageSeq, {
+        frame: frameCount - 1,
+        snap: "frame",
+        ease: `none`,
+        onUpdate: render,
+      })
+      .to(canvas, {
+        transform: "scale(.2)",
+        opacity: 0,
+        top: "11%",
+        left: "-2.3%",
+        width: "88%",
+      });
+
+    images[1].onload = render;
+
+    function render() {
+      scaleImage(images[imageSeq.frame], context!);
+    }
+
+    ScrollTrigger.create({
+      trigger: "#canvas-card",
+      pin: true,
+      scroller: `body`,
+      start: `top top`,
+      end: `200% top`,
+    });
+
+    return () => {
+      window.removeEventListener("resize", resizeCanvas);
+      ScrollTrigger.getAll().forEach((st) => st.kill());
+    };
+  }, []);
+  return (
+    <>
+      <canvas ref={canvasRef}></canvas>
+    </>
+  );
+};
+
+function files(index: number): string {
+  return `./canvas/canvas (${index + 1}).png`;
+}
+
+function scaleImage(img: HTMLImageElement, ctx: CanvasRenderingContext2D) {
+  const canvas = ctx.canvas;
+  const hRatio = canvas.width / img.width;
+  const vRatio = canvas.height / img.height;
+  const ratio = Math.max(hRatio, vRatio);
+  const centerShift_x = (canvas.width - img.width * ratio) / 2;
+  const centerShift_y = (canvas.height - img.height * ratio) / 2;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    img,
+    0,
+    0,
+    img.width,
+    img.height,
+    centerShift_x,
+    centerShift_y,
+    img.width * ratio,
+    img.height * ratio
+  );
+}
+
+export default CanvasVideo;
---
+++ src/components/ChatImage.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const ChatImage = () => {
+  return (
+    <>
+      <img src="/imgs/iPad Pro 11 Inches.png" alt="Iphone" />
+      <div id="chat">
+        <div className="message">
+          <div className="wrap">
+            <p>
+              Hi, AI Assistant. I&apos;ve just recorded a cough sound from
+              Patient X. Can you analyze it for me?
+            </p>
+          </div>
+        </div>
+        <div className="message">
+          <div className="wrap">
+            <p>
+              Of course, Doctor. Please upload the audio file, and I&apos;ll
+              start the analysis.
+            </p>
+          </div>
+          <div className="profile"></div>
+        </div>
+        <div className="message">
+          <div className="wrap">
+            <img src="/imgs/canvas-cover.png" alt="Cover" />
+          </div>
+        </div>
+        <div className="message">
+          <div className="wrap">
+            <p>Thank you. Analyzing the cough sound now…</p>
+          </div>
+          <div className="profile"></div>
+        </div>
+        <div className="message">
+          <div className="wrap">
+            <h5>✦ Based on this analysis, these are the next steps</h5>
+            <p>
+              I recommend performing a sputum test for TB to confirm the
+              diagnosis. Additionally, a chest X-ray might be useful to assess
+              any lung.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ChatImage;
